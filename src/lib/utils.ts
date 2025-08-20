a57a1ad973dfdea442a717834bbcd18e29577ea3
3jun <kk3june@gmail.com>
2025-08-20 18:30:38 +0900

    feat. 영업자, 기술사 계정 정보 타입 및 포맷팅 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatBusinessNumber = (value: string) => {
+  return value
+    .replace(/[^0-9]/g, "")
+    .replace(/([0-9]{3})([0-9]{2})([0-9]{5})/g, "$1-$2-$3");
+};
+
+export const formatPhoneNumber = (value: string) => {
+  return value
+    .replace(/[^0-9]/g, "")
+    .replace(
+      /(^02.{0}|^01.{1}|^0[3-6][0-9].{0}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/g,
+      "$1-$2-$3"
+    );
+};
