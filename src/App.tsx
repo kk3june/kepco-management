fce82e9cecd3d47555ec2dd29dc4689f72d397a6
3jun <kk3june@gmail.com>
2025-09-05 12:09:10 +0900

    Fix React Router nested routes structure with Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { CustomerDetail } from "@/pages/CustomerDetail";
 import { Customers } from "@/pages/Customers";
 import { Engineers } from "@/pages/Engineers";
 import { Salesmans } from "@/pages/Salesmans";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, Outlet } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./lib/auth";
 import { Dashboard } from "./pages/Dashboard";
@@ -17,35 +17,35 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route
-            path="/*"
+            path="/"
             element={
               <ProtectedRoute>
                 <Layout>
-                  <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route
-                      path="/salesmans"
-                      element={
-                        <ProtectedRoute requiredRole="ADMIN">
-                          <Salesmans />
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path="/engineers"
-                      element={
-                        <ProtectedRoute requiredRole="ADMIN">
-                          <Engineers />
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route path="/customers" element={<Customers />} />
-                    <Route path="/customers/:id" element={<CustomerDetail />} />
-                  </Routes>
+                  <Outlet />
                 </Layout>
               </ProtectedRoute>
             }
-          />
+          >
+            <Route index element={<Dashboard />} />
+            <Route
+              path="salesmans"
+              element={
+                <ProtectedRoute requiredRole="ADMIN">
+                  <Salesmans />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="engineers"
+              element={
+                <ProtectedRoute requiredRole="ADMIN">
+                  <Engineers />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="customers" element={<Customers />} />
+            <Route path="customers/:id" element={<CustomerDetail />} />
+          </Route>
         </Routes>
       </Router>
     </AuthProvider>
